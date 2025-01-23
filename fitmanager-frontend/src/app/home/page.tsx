a69d69f24f6ddb1761346dbff58c876a46c2ecbe
Tomas Laprovitta <tlaprovitta@gmail.com>
2025-01-23 12:16:56 -0300

    Responsive Design && NewLogo

--- fitmanager-frontend/src/app/home/page.tsx
+++ fitmanager-frontend/src/app/home/page.tsx
@@ -6,62 +6,68 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col w-full">
-
       <div className="relative w-full h-[500px]">
-      <Image
-        src="/ImgHome.jpeg"
-        alt="home"
-        className="w-full h-full object-cover"
-        width={3000}
-        height={2000}
-      />
+        <Image
+          src="/ImgHome.jpeg"
+          alt="home"
+          className="w-full h-full object-cover"
+          width={3000}
+          height={2000}
+        />
 
-      <div className="absolute inset-0 bg-gray-500 opacity-50"></div>
+        <div className="absolute inset-0 bg-gray-500 opacity-50"></div>
 
-      <div className="absolute inset-0 flex items-center justify-center bg-gray-400/20">
-        <div className="flex flex-col items-center justify-center gap-4">
-          <h1 className="text-black text-center text-6xl font-bold">FitManager</h1>
-          <div className="font-serif">
-            <Link
-            className="w-[10rem] text-center m-2 text-black p-2 bg-gray-300 hover:bg-gray-200 transition duration-300 ease rounded-lg border-[1px] border-gray-400"
-            href="/aboutus"
-            >
-            ¿Quienes somos?
-            </Link>
-            <Link
-            className="w-[12rem] text-center p-2 mr-4 bg-black text-white hover:bg-gray-950 hover:border-gray-950 transition duration-300 ease rounded-lg border-[1px] border-black"
-            href="/login"
-            >
-            Ingresar
-            </Link>
+        <div className="absolute inset-0 flex items-center justify-center bg-gray-400/20">
+          <div className="flex flex-col items-center justify-center gap-4">
+            <Image
+              src="/LogoFitManager.png"
+              alt="logo"
+              width={200}
+              height={200}
+            />
+            <h1 className="text-black text-center text-5xl font-bold">
+              FitManager
+            </h1>
+            <div className="font-serif">
+              <Link
+                className="w-[10rem] text-center m-2 text-black p-2 bg-gray-300 hover:bg-gray-200 transition duration-300 ease rounded-lg border-[1px] border-gray-400"
+                href="/aboutus"
+              >
+                ¿Quienes somos?
+              </Link>
+              <Link
+                className="w-[12rem] text-center p-2 mr-4 bg-black text-white hover:bg-gray-950 hover:border-gray-950 transition duration-300 ease rounded-lg border-[1px] border-black"
+                href="/login"
+              >
+                Ingresar
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
       </div>
 
       <div>
         <div className="m-10 font-bold text-2xl">
           <h1>Entrenadores</h1>
-          <hr className="ml-4 border-b-1 border-black w-[9rem]"/>
+          <hr className="ml-4 border-b-1 border-black w-[9rem]" />
         </div>
         <div>
           <div className="flex justify-center items-center">
             <div className="grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-1 gap-4 mb-20">
               {coaches.map((coach) => (
-                <CoachCard 
-                  key={coach.id} 
+                <CoachCard
+                  key={coach.id}
                   id={coach.id}
-                  name={coach.name} 
-                  img={coach.img} 
-                  description={coach.description} 
-                  email={coach.email} 
+                  name={coach.name}
+                  img={coach.img}
+                  description={coach.description}
+                  email={coach.email}
                 />
               ))}
             </div>
           </div>
         </div>
       </div>
-
     </div>
   );
 }
--- fitmanager-frontend/src/app/users/[slug]/page.tsx
+++ fitmanager-frontend/src/app/users/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { getUserInfo } from "@/helpers/getUserInfo";
 import Link from "next/link";
 import { IoIosArrowBack } from "react-icons/io";
+import { BiSolidError } from "react-icons/bi";
 
 export default async function User({
   params,
@@ -14,114 +15,82 @@ export default async function User({
 
   return userData ? (
     <div className="flex flex-col w-full justify-center items-center mt-24">
-        <div className="flex flex-col w-full items-center justify-between">
+      {/* Header */}
+      <div className="flex flex-col w-full items-center justify-between">
         <div className="flex w-10/12 justify-start items-start">
-            <Link  className="border-[1px] p-1 border-gray-200 rounded-lg hover:bg-gray-50 transition duration-300 ease" href="/administration">
+          <Link
+            className="border-[1px] p-1 border-gray-200 rounded-lg hover:bg-gray-50 transition duration-300 ease"
+            href="/administration"
+          >
             <IoIosArrowBack size={25} />
-            </Link>
+          </Link>
         </div>
         <div className="flex justify-center items-center w-full">
-            <h1 className="text-2xl font-bold mb-6">Información del Usuario</h1>
+          <h1 className="text-2xl font-bold mb-6">Información del Usuario</h1>
         </div>
-        </div>
+      </div>
 
       <div className="w-10/12 flex-col border-[1px] border-gray-200 rounded-lg mb-10 shadow-md">
         <div className="w-full flex flex-col p-2 border-b-[1px] border-gray-200 bg-gray-50">
-          <h1 className="text-xl font-bold text-center ">{userData.name}</h1>
-          </div >
-          <div className="flex w-full justify-center items-center p-4">
-          <div className="flex flex-col w-full md:w-6/12">
-            <ul>
-              <li className="flex">
-                <h1 className="font-bold">ID:</h1>
-              </li>
-              <li className="flex">
-                <h1 className="font-bold">Nombre:</h1>
-              </li>
-              <li className="flex">
-                <h1 className="font-bold">Estado:</h1>
-              </li>
-              <li className="flex">
-                <h1 className="font-bold">Teléfono:</h1>
-              </li>
-              <li className="flex">
-                <h1 className="font-bold">Nivel:</h1>
-              </li>
-              <li className="flex">
-                <h1 className="font-bold">Email:</h1>
-              </li>
-            </ul>
-          </div>
-
-          <div className="flex flex-col w-full md:w-6/12">
-            <ul>
-              <li className="flex">
-                <p>{userData.id}</p>
-              </li>
-              <li className="flex">
-                <p>{userData.name}</p>
-              </li>
-              <li className="flex">
-                <p className={userData.isActive ? "text-green-500 font-bold" 
-                : "text-red-500 font-bold"}
-                >{userData.isActive ? "Activo" : "Inactivo"}</p>
-              </li>
-              <li className="flex">
-                <p>{userData.phone}</p>
-              </li>
-              <li className="flex">
-                <p>A definir</p>
-              </li>
-              <li className="flex">
-                <p>{userData.email}</p>
-              </li>
-            </ul>
-          </div>
-
-          <div className="flex flex-col w-full md:w-6/12">
-            <ul>
-              <li className="flex">
-                <h1 className="font-bold">País:</h1>
-              </li>
-              <li className="flex">
-                <h1 className="font-bold">Ciudad:</h1>
-              </li>
-              <li className="flex">
-                <h1 className="font-bold">Dirección:</h1>
-              </li>
-              <li className="flex">
-                <h1 className="font-bold">Rol:</h1>
-              </li>
-              <li className="flex">
-                <h1 className="font-bold">Fecha de Alta:</h1>
-              </li>
-            </ul>
-          </div>
-
-          <div className="flex flex-col w-full md:w-6/12">
-            <ul>
-              <li className="flex">
-                <p>{userData.country}</p>
-              </li>
-              <li className="flex">
-                <p>{userData.city}</p>
-              </li>
-              <li className="flex">
-                <p>{userData.address}</p>
-              </li>
-              <li className="flex">
-                <p>{userData.rol}</p>
-              </li>
-              <li className="flex">
-                <p>{userData.entry_date.toLocaleDateString()}</p>
-              </li>
-            </ul>
-        </div>
+          <h1 className="text-xl font-bold text-center">{userData.name}</h1>
         </div>
 
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
+          <div className="flex flex-col">
+            {[
+              { label: "ID", value: userData.id },
+              { label: "Nombre", value: userData.name },
+              {
+                label: "Estado",
+                value: (
+                  <span
+                    className={
+                      userData.isActive
+                        ? "text-green-500 font-bold"
+                        : "text-red-500 font-bold"
+                    }
+                  >
+                    {userData.isActive ? "Activo" : "Inactivo"}
+                  </span>
+                ),
+              },
+              { label: "Teléfono", value: userData.phone },
+              { label: "Nivel", value: "A definir" },
+              { label: "Email", value: userData.email },
+            ].map(({ label, value }, index) => (
+              <div key={index} className="grid grid-cols-2 gap-4">
+                <h1 className="font-bold">{label}:</h1>
+                <p>{value}</p>
+              </div>
+            ))}
+          </div>
+
+          <div className="flex flex-col">
+            {[
+              { label: "País", value: userData.country },
+              { label: "Ciudad", value: userData.city },
+              { label: "Dirección", value: userData.address },
+              { label: "Rol", value: userData.rol },
+              {
+                label: "Fecha de Alta",
+                value: userData.entry_date.toLocaleDateString(),
+              },
+            ].map(({ label, value }, index) => (
+              <div key={index} className="grid grid-cols-2 gap-4">
+                <h1 className="font-bold">{label}:</h1>
+                <p>{value}</p>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   ) : (
-    <div>Usuario no Encontrado</div>
+    <div className="flex w-4/12 flex-col border-[1px] justify-center items-center mx-auto border-gray-200 rounded-lg mb-10 shadow-md mt-20">
+      <div className="flex p-4 font-bold">
+        <BiSolidError size={20} />
+        <h1 className="ml-2">Usuario no encontrado</h1>
+      </div>
+    </div>
   );
 }
