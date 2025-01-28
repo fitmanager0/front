def5ee86d2167db90c9145576f2e2c48ab248c1f
Tomas Laprovitta <tlaprovitta@gmail.com>
2025-01-28 16:34:44 -0300

    UserInfo fix

--- fitmanager-frontend/src/app/users/[slug]/page.tsx
+++ fitmanager-frontend/src/app/users/[slug]/page.tsx
@@ -1,12 +1,8 @@
 import ProtectedRoute from "@/components/ProtectedRoute/ProtectedRoute";
 import User from "./User";
 
-export default async function UserPage({
-  params,
-}: {
-  params: { slug: string }; 
-}) {
-  const resolvedParams = params; 
+export default async function UserPage({ params }: { params: Promise<{ slug: string }> }) {
+  const resolvedParams = await params; 
   return (
     <ProtectedRoute>
       <User params={resolvedParams} />
