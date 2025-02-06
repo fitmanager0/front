29488064a32a8b7696456473b018af586969b3cc
Danel Torchiari <danel.t@hotmail.com>
2025-02-06 13:03:58 -0300

    envmp

--- fitmanager-frontend/src/app/(Dashboard)/dashboard/user/payments/mercado-pago/MercadoPago.tsx
+++ fitmanager-frontend/src/app/(Dashboard)/dashboard/user/payments/mercado-pago/MercadoPago.tsx
@@ -68,7 +68,7 @@ export default function MercadoPago() {
       const userId = decodedToken.id
 
       const response = await axios.post(
-        `${process.env.NEXT_PUBLIC_API_URL}/payment/create-preference`, 
+        `${process.env.NEXT_PUBLIC_API_URL}/payment/create-preference`,
        {
           turno: {
             service: plan.title,
