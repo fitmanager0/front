50e2969f5cf8ba78f4d9e196b8f6289411455195
Danel Torchiari <danel.t@hotmail.com>
2025-02-06 13:01:55 -0300

    envmercadopago

--- fitmanager-frontend/src/app/(Dashboard)/dashboard/user/payments/mercado-pago/MercadoPago.tsx
+++ fitmanager-frontend/src/app/(Dashboard)/dashboard/user/payments/mercado-pago/MercadoPago.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
+import { jwtDecode } from "jwt-decode"
 
 // Initialize MercadoPago with your public key
 initMercadoPago('APP_USR-555d9faa-7d27-4cb6-9ea5-326f18f1b89c');
@@ -63,30 +64,36 @@ export default function MercadoPago() {
         return;
       }
 
-      const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/payment/create-preference`,
-        {
+      const decodedToken: any = jwtDecode(token)
+      const userId = decodedToken.id
+
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/payment/create-preference`, 
+       {
           turno: {
             service: plan.title,
             price: plan.price,
           },
+          userId: userId, // Añadir el ID del usuario a la solicitud
+          userEmail: decodedToken.email, // Añadir el email del usuario si es necesario
         },
         {
           headers: {
             Authorization: `Bearer ${token}`,
           },
-        }
+        },
       );
 
       if (response.data && response.data.id) {
-        setPreferenceId(response.data.id);
-        setSelectedPlan(plan);
-        console.log("Preferencia creada exitosamente:", response.data);
+        setPreferenceId(response.data.id)
+        setSelectedPlan(plan)
+        console.log("Preferencia creada exitosamente:", response.data)
       } else {
-        throw new Error('La respuesta del servidor no contiene un ID de preferencia válido');
+        throw new Error("La respuesta del servidor no contiene un ID de preferencia válido")
       }
     } catch (error) {
-      console.error("Error al crear la preferencia:", error);
-      setError('Error al crear la preferencia de pago');
+      console.error("Error al crear la preferencia:", error)
+      setError("Error al crear la preferencia de pago")
     }
   };
 
